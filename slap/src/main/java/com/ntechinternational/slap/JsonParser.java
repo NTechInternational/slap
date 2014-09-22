6c1bdcd33eee353de10d90c44192851b8c36ef45
sundipp@hotmail.com <sundipp@hotmail.com>
2014-09-22 09:40:38 -0400

    json

--- slap/src/main/java/com/ntechinternational/slap/JsonParser.java
+++ slap/src/main/java/com/ntechinternational/slap/JsonParser.java
@@ -6,6 +6,12 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URL;
 import java.util.Iterator;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -35,32 +41,14 @@ public class JsonParser {
 		JSONObject jsonObject = (JSONObject) jsonParser.parse(r);
 
 		// get a String from the JSON object
-		String firstName = (String) jsonObject.get("firstname");
-		System.out.println("The first name is: " + firstName);
+		String doc = (String) jsonObject.get("doc");
+		System.out.println("The doc name is: " + doc);
 
 		// get a number from the JSON object
 		long id =  (long) jsonObject.get("id");
 		System.out.println("The id is: " + id);
 
-		// get an array from the JSON object
-		JSONArray lang= (JSONArray) jsonObject.get("languages");
 		
-		// take the elements of the json array
-		for(int i=0; i<lang.size(); i++){
-			System.out.println("The " + i + " element of the array: "+lang.get(i));
-		}
-		Iterator i = lang.iterator();
-
-		// take each value from the json array separately
-		while (i.hasNext()) {
-			JSONObject innerObj = (JSONObject) i.next();
-			System.out.println("language "+ innerObj.get("lang") + 
-					" with level " + innerObj.get("knowledge"));
-		}
-		// handle a structure into the json object
-		JSONObject structure = (JSONObject) jsonObject.get("job");
-		System.out.println("Into job structure, name: " + structure.get("name"));
-
 	} catch (FileNotFoundException ex) {
 		ex.printStackTrace();
 	} catch (IOException ex) {
