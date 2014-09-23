3e74c2af44bd4d2d439ff2a6ba3de28f5c6d16b5
sundipp@hotmail.com <sundipp@hotmail.com>
2014-09-22 23:26:50 -0400

    merged

---
+++ slap/src/main/java/com/ntechinternational/slap/Question.java
@@ -0,0 +1,50 @@
+package com.ntechinternational.slap;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.HashMap;
+
+@XmlRootElement
+public class Question {
+
+	@XmlElement
+	public long questionID;
+	private HashMap<String,Object> properties = null;
+	
+	public Question(){
+
+		this.properties = new HashMap<String,Object>();
+		
+	}
+	
+	public Question(long questionID){
+		this.properties = new HashMap<String,Object>();
+
+		this.id = questionID;
+	}
+	
+	public HashMap<String,Object> getProperties(){
+		return properties;
+	}
+	public Object getPropertyVal(String propertyName){
+		
+		if (this.properties!=null && this.properties.containsKey(propertyName))
+			return properties.get(propertyName);
+		
+		return null;
+	}
+	public Question put(String attributeName, String value){
+		if(attributeName.equals("businessModel")){
+			this.businessModel = value;
+		}
+		else if(attributeName.equals("id")){
+			this.id = Long.parseLong(value);
+		}
+		else{
+			System.err.println("Ignoring invalid parameter of type " + attributeName);
+		}
+		
+		return this;
+
+	}
+}
