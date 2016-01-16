6ff6d1c7d1f162282e71b766d5fef80a6f3a4e61
Pradhanta Bhandari <pradhanta.bhandari@gmail.com>
2016-01-17 00:34:57 +0545

    Added code to support back interaction, done and startover interaction

--- python/slap/api/core.py
+++ python/slap/api/core.py
@@ -5,6 +5,10 @@ from api.transformers import Transformer, ItemTransformer
 
 
 class Interaction:
+    """
+
+    """
+
     VISITOR_PARAM_KEY = 'visitorid'
     APP_ID_PARAM_KEY = 'appid'
     APP_KEY_PARAM_KEY = 'appkey'
@@ -13,11 +17,19 @@ class Interaction:
     VARIABLE_PARAM_KEY = 'var'
     FACETS_PARAM_KEY = 'facet'
     QUESTION_PARAM_KEY = 'questionid'
+    FINAL_TEXT_PARAM_KEY = 'text'
+
+    class Interactions:
+        SELECT = 'select'
+        SUBMIT = 'submit'
+        DONE = 'done'
+        START_OVER = 'startover'
+        BACK = 'back'
 
     def __init__(self, request = None, auth_provider = None):
         self.request = request
         self.user = Visitor()
-        self.response = None
+        self.response = SlapResponse(self.user)
         self.has_error = False
         self.auth_provider = auth_provider
 
@@ -29,7 +41,7 @@ class Interaction:
         if self.VISITOR_PARAM_KEY in self.request.GET:
             visitor_id = self.request.GET[self.VISITOR_PARAM_KEY]
             self.user = Visitor.get_by_visitor_id(visitor_id)
-            if self.user != None:
+            if self.user is not None:
                 return True
 
         self.has_error = True
@@ -68,16 +80,17 @@ class Interaction:
         #  "startOver" => User session Start over				[Interaction 4]
         if self.INTERACTION_PARAM_KEY in self.request.GET:
             interaction = self.request.GET[self.INTERACTION_PARAM_KEY].lower()
-            if interaction == 'select':
-                self.select()
-            elif interaction == 'submit':
-                self.submit()
-            elif interaction == 'done':
-                self.response = Error('Done interaction in progress')
-            elif interaction == 'startover':
-                self.start_over()
-            elif interaction == 'back':
-                self.response = Error('Back interaction in progress')
+
+            if interaction == self.Interactions.SELECT:
+                self.select_interaction()
+            elif interaction == self.Interactions.SUBMIT:
+                self.submit_interaction()
+            elif interaction == self.Interactions.DONE:
+                self.done_interaction()
+            elif interaction == self.Interactions.START_OVER:
+                self.start_over_interaction()
+            elif interaction == self.Interactions.BACK:
+                self.back_interaction()
             else:
                 self.default_interaction()
         else:
@@ -89,7 +102,7 @@ class Interaction:
         """
         if self.user.selected_challenge_id != None:
             #Challenge is selected so we need to switch to challenge interaction
-            self.select()
+            self.select_interaction()
         else:
             #check if facets were previously submitted
             #if they have been we select based on the facet
@@ -102,7 +115,7 @@ class Interaction:
             Transformer.convert_items_to_proper_format(items)
             self.response.set_items(items)
 
-    def select(self):
+    def select_interaction(self):
         """
         select interaction that selects an item/challenge
         Identifies the variables that don't have a value i.e. a user hasn't provided a value or default doesn't exist
@@ -113,6 +126,9 @@ class Interaction:
             challenge_id = self.request.GET[self.CHALLENGE_PARAM_KEY]
             (success, challenge_id) = self.__parse_int(challenge_id)
             if success:
+                # save the old_challenge_id so that we can save it in interaction log
+                old_challenge_id = self.user.selected_challenge_id
+
                 self.response = SlapResponse(self.user)
                 # a. save the selected transaction in selectedChallenge
                 self.user.select_challenge(challenge_id)
@@ -142,15 +158,17 @@ class Interaction:
                     questions.extend(SolrQuery(query_params = query_params).query())
 
 
-                # e. return response
+                # e. Return response
                 Transformer.convert_answers_to_proper_format(questions)
                 self.response.set_questions(questions)
 
+                # Log the current interaction
+                self._push_interaction(self.Interactions.SELECT, {self.CHALLENGE_PARAM_KEY : old_challenge_id})
             else:
                 self.has_error = True
                 self.response = Error('Invalid challenge was selected')
 
-    def submit(self):
+    def submit_interaction(self):
         """
         Submit interaction stores the given submission provided by the user. Submission can be answer to a
         particular variable or selection of a facet.
@@ -163,6 +181,10 @@ class Interaction:
             facets = {}
             has_submission = False
 
+            # Old submission will be stored incase we need to undo
+            old_submission = Question.get_submissions_for(self.user.visitor_id, question_id)
+            old_submission = old_submission[0].to_json() if len(old_submission) == 1 else None
+
             if self.VARIABLE_PARAM_KEY in getRequest:
                 variables = self.__get_name_value_from_string(getRequest[self.VARIABLE_PARAM_KEY])
                 has_submission = True # a user could have submitted either a variable's value or facets
@@ -174,12 +196,14 @@ class Interaction:
             # there is no point saving if nothing has been submitted or selected
             if has_submission:
                 self.user.save_submission(question_id, variables, facets)
+                self._push_interaction(self.Interactions.SUBMIT, {self.QUESTION_PARAM_KEY: question_id,
+                                                              'submission': old_submission})
+
 
             answered_variables = self.user.get_answered_variables()
             selected_facets = self.user.get_selected_facets()
 
             # TODO: Code Refactor, too many duplicates, move code Question and Item Query block to a common method
-            # a lot of code such as getting the challenge and question is duplicate
 
             self.response = SlapResponse(visitor = self.user)
 
@@ -193,7 +217,7 @@ class Interaction:
 
             if item_id is not None:
                 items = SolrQuery(query_type = SolrQuery.CHALLENGE_QUERY,
-                                  query_params = {'rows' : 1, 'id' : str(challenge_id), 'businessmodel' : '*'}).query()
+                                  query_params = {'rows' : 1, 'id' : str(item_id), 'businessmodel' : '*'}).query()
                 answered_variables = self.user.get_answered_variables()
                 ItemTransformer(items = items).transform(answered_variables = answered_variables)
                 self.response.set_items(items)
@@ -229,7 +253,7 @@ class Interaction:
                 Transformer.convert_answers_to_proper_format(questions)
                 self.response.set_questions(questions)
 
-    def start_over(self):
+    def start_over_interaction(self):
         """
         Resets the current session and starts a new one
         :return:
@@ -239,13 +263,42 @@ class Interaction:
         # log = InteractionLog.get_interaction(self.user.visitor_id, collection)
         # # log.push('asd', 'Hello', collection)
         # last_interaction = log.pop(collection)
+        self.user.reset_session()
+        self.default_interaction()
 
-        session_info = VisitorSessionInfo(visitor=self.user, submitted_question=Question.get_submissions_for(self.user.visitor_id), final_text='Hello world!!!')
+    def done_interaction(self):
+        text = ''
+        if self.FINAL_TEXT_PARAM_KEY in self.request.GET:
+            text = self.request.GET[self.FINAL_TEXT_PARAM_KEY]
 
-        session_info.save(True)
+        # TODO: consider the case where new variables and facets are submitted too.
 
-        # self.user.reset_session()
-        # self.default_interaction()
+        self.user.save_session()
+
+        self.default_interaction()
+
+    def back_interaction(self):
+        Meta = InteractionLog.Meta
+        last_interaction = self._pop_interaction()
+        if last_interaction is not None:
+            interaction_type = last_interaction[Meta.INTERACTION_TYPE_KEY]
+            if interaction_type == self.Interactions.SUBMIT:
+                # undo submit interaction
+                question_id = last_interaction[InteractionLog.Meta.OLD_VALUE_KEY][self.QUESTION_PARAM_KEY]
+                submission = last_interaction[InteractionLog.Meta.OLD_VALUE_KEY]['submission']
+
+                if submission is None:
+                    self.user.delete_submission(question_id)
+                else:
+                    self.user.save_submission(question_id,
+                                              submission[Question.Meta.VARIABLES_KEY],
+                                              submission[Question.Meta.FACETS_KEY])
+            elif interaction_type == self.Interactions.SELECT:
+                # undo select interaction
+                self.user.select_challenge(last_interaction[InteractionLog.Meta.OLD_VALUE_KEY][self.CHALLENGE_PARAM_KEY])
+
+        #perform default interaction
+        self.default_interaction()
 
     def __get_name_value_from_string(self, variable = ''):
         """
@@ -293,4 +346,13 @@ class Interaction:
 
         return Error('Nothing has been initialized').to_json()
 
+    def _push_interaction(self, type, old_value):
+        collection = InteractionLog.get_collection()
+        log = InteractionLog.get_interaction(self.user.visitor_id, collection)
+        log.push(type, old_value, collection)
 
+    def _pop_interaction(self):
+        collection = InteractionLog.get_collection()
+        log = InteractionLog.get_interaction(self.user.visitor_id, collection)
+
+        return log.pop(collection)
